727bf6cb29e5372ad0508ac78cbe14195aad95b2
dukAdobe <duk@adobe.com>
2024-09-15 09:00:00 +0530

    Removed base

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 // vite.config.js
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [
-    react()
-  ],
-  base: '/riya-portfolio/'
+  plugins: [react()],
 });
